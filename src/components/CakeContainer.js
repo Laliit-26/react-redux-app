c8fb213c86e03b1f2fed83ea017facb9611d6aff
lalit <tiwarilalit2601@gmail.com>
2021-09-26 16:35:18 +0530

    provider connect state and dispatch with component

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { buycake } from '../redux/cakes/CakeAction'
 
-function CakeContainer
-() {
+function CakeContainer(props) {
     return (
         <div>
-             <h2>number of cakes</h2>
-      <button>buy cake</button>
+            <h2>number of cakes:{props.numofcakes}</h2>
+      <button onClick={props.buycake}>buy cake</button>
         </div>
     )
 }
+const mapstatetoprops = state => {
+    return {
+        numofcakes:state.numofcakes
+    }
+}
 
-export default CakeContainer;
+const mapdispatchtoprops = dispatch => {
+    return {
+        buycake: () =>{dispatch(buycake())}
+    }
+}
+
+export default connect(mapstatetoprops,mapdispatchtoprops)(CakeContainer);
 
